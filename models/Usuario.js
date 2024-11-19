3f2feb1eabbc51391cecafca18c91ebecd5154e4
valenlopezzz04 <140413463+valenlopezzz04@users.noreply.github.com>
2024-11-18 21:40:55 -0500

    crear archivo usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -4,7 +4,7 @@ const UsuarioSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'standard'], default: 'standard' } // Campo de rol con valor por defecto
+    role: { type: String, enum: ['admin', 'standard'], default: 'standard' } 
 });
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);

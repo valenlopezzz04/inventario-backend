b14bda6e5be311e92ed164328801dc93a66d4748
valenlopezzz04 <140413463+valenlopezzz04@users.noreply.github.com>
2024-11-21 22:36:52 -0500

    actualizar jestconfig

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,12 @@
 module.exports = {
-    // Directorio raíz del proyecto
-    rootDir: "./",
-  
-    // Extensiones de archivo que Jest debería reconocer
-    moduleFileExtensions: ["js", "json", "jsx", "node"],
-  
-    // Patrones para incluir pruebas
-    testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  
-    // Excluir directorios y archivos específicos de las pruebas
-    testPathIgnorePatterns: [
-      "<rootDir>/coverage/", // Excluye el directorio de coverage
-      "<rootDir>/node_modules/", // Excluye node_modules
-      "<rootDir>/jest.config.js", // Excluye jest.config.js (si lo requiere)
-      "<rootDir>/testconnection.js", // Excluye este archivo específico
-    ],
-  
-    // Configuración para el reporte de cobertura
     collectCoverage: true,
     collectCoverageFrom: [
-      "**/*.js", // Incluir todos los archivos .js
-      "!<rootDir>/coverage/**", // Excluir todo el directorio coverage
-      "!<rootDir>/jest.config.js", // Excluir jest.config.js
-      "!<rootDir>/node_modules/**", // Excluir node_modules
-      "!<rootDir>/testconnection.js", // Excluir testconnection.js
+        "**/*.js", // Incluye todos los archivos .js
+        "!**/coverage/**", // Excluye el directorio coverage
+        "!**/node_modules/**", // Excluye node_modules
+        "!testconnection.js", // Excluye testconnection.js
+        "!jest.config.js" // Excluye el archivo de configuración de Jest
     ],
-  
-    // Directorio donde se guardará el reporte de cobertura
     coverageDirectory: "coverage",
-    coverageReporters: ["json", "lcov", "text", "clover"],
-  
-    // Mostrar detalles en la consola
-    verbose: true,
-  
-    // Transformar archivos antes de ejecutarlos
-    transform: {
-      "^.+\\.js$": "babel-jest",
-    },
-  
-    // Ignorar rutas de módulos específicas
-    modulePathIgnorePatterns: ["<rootDir>/coverage/", "<rootDir>/node_modules/"],
-  };
-  
+    testEnvironment: "node",
+};

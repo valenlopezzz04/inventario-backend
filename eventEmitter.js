3cfb5431114aee4b4c22bd31bbd81f30ba2d6bc9
valenlopezzz04 <140413463+valenlopezzz04@users.noreply.github.com>
2024-11-23 00:06:58 -0500

    crear listener para el evento

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,4 +1,10 @@
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
+// Escucha el evento 'stockInsuficiente'
+eventEmitter.on('stockInsuficiente', (data) => {
+    console.log('Evento recibido: stock insuficiente');
+    console.log('Detalles del producto:', data);
+});
+
 module.exports = eventEmitter;
